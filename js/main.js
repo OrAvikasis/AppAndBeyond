377023169037497ebcb55de8e2afca610ddbeaf9
Or Avikasis <orbenra@gmail.com>
2017-12-04 17:10:50 +0200

    fixed animations responsiveness and added h2 headings

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var subtitleElement = document.getElementById("visible-subtitle");
+
+var setSubtitleToDefault = function() {
+ subtitleElement.textContent = subtitleElement.getAttribute("data-default"); 
+};
+
+setSubtitleToDefault();
+
+var blockItemElements = document.getElementsByClassName("block-item");
+
+var setSubtitleFromBlockItem = function(){
+  subtitleElement.textContent = this.getAttribute("data-subtitle");
+}
+for (var element of blockItemElements) {
+  element.addEventListener("mouseenter", setSubtitleFromBlockItem);
+  element.addEventListener("mouseleave", setSubtitleToDefault); 
+}
+
+
